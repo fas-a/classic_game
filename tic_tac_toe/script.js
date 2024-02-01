9cc64dd28d35058eef6e23517062e93e2f3f22da
iamPanda4 <abulkhoirfaris0@gmail.com>
2024-02-01 20:31:57 +0700

    win condition

--- tic_tac_toe/script.js
+++ tic_tac_toe/script.js
@@ -1,16 +1,46 @@
 let a = 0
 let b = 0
-let d = [[0,0,0],[0,0,0],[0,0,0]]
+let d = [0,0,0,0,0,0,0,0,0]
 let c = true
 console.log({d})
-function add(x,y) {
-    if (d[x][y] == 0){
+function add(x) {
+    if (d[x] == 0){
         if (c){
-            d[x][y] = 1
+            d[x] = 1
         } else {
-            d[x][y] = -1
+            d[x] = -1
         }
         console.log({d})
-        c = !c   
+        c = !c
+        let win = check()
+        console.log({win})
+        // if(win == 1){
+        //     console.log("1 win")
+        // } else if(win == -1){
+        //     console.log("-1 win")
+        // }
     }
+}
+function check() {
+    for (let i = 0; i < 3; i++) {
+        console.log("a")
+        if(d[i] == d[i+3] && d[i+3] == d[i+6] && d[i] != 0){
+            return d[i]
+        }
+    }
+    for (let i = 0; i < 9; i+=3) {
+        console.log("b")
+        if(d[i] != 0 && d[i] == d[i+1] && d[i+1] == d[i+2]){
+            return d[i]
+        }
+    }
+    if(d[0] == d[4] && d[4] == d[8] && d[0] != 0){
+        return d[0]
+    }
+    console.log("c")
+    if(d[2] == d[4] && d[4] == d[6] && d[2] != 0){
+        return d[2]
+    }
+    console.log("d")
+    return 0
 }
